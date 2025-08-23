c758c12b3bd40f3c063ca54df979cfccc611c431
Sandhuekroop <ekroop4@gmail.com>
2025-08-24 02:57:03 +0530

    tailwind

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-
+// import {jsx as _jsx} from 'react/jsx-runtime.js'
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App | chai</h1>
+    </div>
+  )
+}
+
+// const reactElement={
+//     type: 'a',
+//     props:{
+//         href: 'https://www.google.com',
+//         target: '_blank'
+//     },
+//     children:'click me to visit google'
+// }
+
+const anotherElement=(
+  <a href="https://www.google.com" target="_blank"> Visit Google </a>
+)
+
+ const anotherUser="chai aur react"
+const reactElement= React.createElement(
+  'a',
+  {href:'https://www.google.com', target: '_blank'},
+  'click me to visit google',
+  anotherElement
+)
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
+    // <MyApp />
+    // MyApp()
+    // anotherElement
+    reactElement,
     <App />
-  </StrictMode>,
+ 
 )
